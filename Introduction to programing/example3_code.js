d65e75c36a09c7f37052031729eb1ce428ce21ad
Md Farukul Islam <mfislam007@yahoo.com>
2020-04-08 01:43:20 +0300

    working file

--- Introduction to programing/example3_code.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//Function to calculate the area of a circle
-function circleArea() {
-    var radius, area;
-    radius = Number(document.getElementById("id1").value);
-    area = Math.PI * radius * radius;
-    document.getElementById("id2").value = area.toFixed(2);
-	document.getElementById("id3").innerHTML = "Result is " + area.toFixed(2);
-}
